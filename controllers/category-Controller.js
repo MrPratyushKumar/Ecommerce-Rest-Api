a1ba8c640b01c0c6d9fa6f50b7ab39fd0387b981
Pratyush Pandey <pratyushkr4148@gmail.com>
2026-03-16 19:21:45 +0530

    Have written the logic of controller function of getAllCategories , getSingleCategoryByID , createNewCategory

---
+++ controllers/category-Controller.js
@@ -0,0 +1,81 @@
+const Category = require("../models/Category.js");
+
+// 1. GET ALL CATEGORIES
+
+const getAllCategories = async (req, res) => {
+  try {
+    // get all active categories
+    const allActiveCategories = await Category.find({ isActive: true });
+    if (allActiveCategories.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: "List of all Active Categories Fetched Successfully",
+        count: allActiveCategories.length,
+        Data: allActiveCategories,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "No Active Categories Found in this E-commerce website",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong! Please try again sometime",
+    });
+  }
+};
+
+const getSingleCategoryByID = async (req , res)=>{
+  try {
+    const getCurrentCategoryId = req.params.id;
+    const categoryDetailsById = await Category.findById(getAllCategories);
+    if(!categoryDetailsById){
+      // Not Found category
+      res.status(404).json({
+        success : false,
+        message : 'Category with current Id is not Found! Please try with different ID'
+      })
+    } 
+    // Found 
+    res.status(200).json({
+      success : true,
+      data : categoryDetailsById
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong! Please try again sometime",
+    });
+  }
+}
+
+const createNewCategory = async (req , res)=> {
+  try {
+    const newCategoryFormData = req.body;
+    const newlyCreatedCategory = await Category.create(newCategoryFormData);
+    if(!newlyCreatedCategory){
+      // not created 
+      res.status(404).json({
+        success : false,
+        message:'does not get Form data from req.body'
+      })
+    }
+    // created
+    res.status(202).json({
+      success : true,
+      message : "Category added successfully",
+      data : newlyCreatedCategory
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something Went Wrong! Please try again sometime",
+    });
+  }
+}
+
